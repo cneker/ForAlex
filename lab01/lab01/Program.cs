8568d636e432c4bfb86c839260821b5e3b533885
cneker <staskotashevich@gmail.com>
2022-08-23 17:06:11 +0300

    added first lab

---
+++ lab01/lab01/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace lab01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main thread started");
+            Console.Write("Enter n: ");
+            int.TryParse(Console.ReadLine(), out int n);
+
+            var task = new Task<double>(() =>
+            {
+                Console.WriteLine("Task started");
+                var res = Calculate(n);
+                Console.WriteLine("Task ended");
+                return res;
+            });
+            task.Start();
+            Console.WriteLine("Result: {0}", task.Result);
+            task.Wait();
+            Console.WriteLine("Main thread ended");
+        }
+
+        static double Calculate(int n)
+        {
+            double sum = 0;
+            for(int k = 0; k <= n; k++)
+            {
+                var temp = 1 / Math.Pow(2, k);
+                Console.WriteLine(temp);
+                sum += temp;
+                Thread.Sleep(1000);
+            }
+
+            return sum;
+        }
+    }
+}
