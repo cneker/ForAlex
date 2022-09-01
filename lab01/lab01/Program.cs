465ac7eaa3a30d9a62ff5a6a30efec124b907a71
cneker <staskotashevich@gmail.com>
2022-09-01 22:02:33 +0300

    added comments for lab1

--- lab01/lab01/Program.cs
+++ lab01/lab01/Program.cs
@@ -7,7 +7,7 @@
             Console.WriteLine("Main thread started");
             Console.Write("Enter n: ");
             int.TryParse(Console.ReadLine(), out int n);
-
+            //создаем таск, высчитывающий значение
             var task = new Task<double>(() =>
             {
                 Console.WriteLine("Task started");
@@ -15,12 +15,14 @@
                 Console.WriteLine("Task ended");
                 return res;
             });
+            //стартуем
             task.Start();
             Console.WriteLine("Result: {0}", task.Result);
+            //ждем, пока отработает
             task.Wait();
             Console.WriteLine("Main thread ended");
         }
-
+        //алгоритм подсчета
         static double Calculate(int n)
         {
             double sum = 0;
